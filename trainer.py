f7a881089efdacfeda1f79f43d92021e6cf096df
travishu <travisergodic1993@gmail.com>
2022-06-04 14:47:58 +0800

    revise best_score code

--- trainer.py
+++ trainer.py
@@ -30,7 +30,7 @@ class Trainer:
 
         for epoch in range(1, num_epoch+1):
             print(f"Epoch [{epoch}/{num_epoch}] train_loss:{self._training_step(model, train_loader, self.criterion)}")
-            val_loss = self._validation_step(model, validation_loader, self.criterion, self.metric_dict)
+            val_loss, metric_eval_dict = self._validation_step(model, validation_loader, self.criterion, self.metric_dict)
             # save model 
             if epoch % save_config["freq"] == 0:
                 if isinstance(model, nn.DataParallel): 
@@ -38,8 +38,8 @@ class Trainer:
                 else: 
                     torch.save(model, save_config["path"])
             # save best model
-            if best_performance < -val_loss:
-                best_performance, best_epoch = -val_loss, epoch
+            if best_performance < metric_eval_dict[save_config['metric']]:
+                best_performance, best_epoch = metric_eval_dict[save_config['metric']], epoch
                 if isinstance(model, nn.DataParallel): 
                     torch.save(model.module, save_config["best_path"])
                 else: 
@@ -48,7 +48,7 @@ class Trainer:
                 self.scheduler.step(val_loss.item())
             else: 
                 self.scheduler.step()
-        print(f"Best loss :{-best_performance} at {epoch} epoch.")
+        print(f"Best performance :{best_performance} at {epoch} epoch.")
 
     def _training_step(self, model, train_loader, criterion): 
         model.train()
@@ -68,9 +68,7 @@ class Trainer:
     @torch.no_grad()
     def _validation_step(self, model, validation_loader, criterion, metric_dict): 
         model.eval()
-        test_loss = 0
-        metric_eval_dict = {k:0 for k in metric_dict}
-
+        val_loss = 0
         pred_list = []
         target_list = []
 
@@ -80,14 +78,17 @@ class Trainer:
 
             predictions = model(data)
             loss = criterion(predictions, targets)
-            test_loss += loss.item() * targets.size(0)
+            val_loss += loss.item() * targets.size(0)
         
             pred_list.extend(predictions.argmax(dim=1).tolist())
             target_list.extend(targets.argmax(dim=1).tolist())
-        test_loss /= len(pred_list)
+        val_loss /= len(pred_list)
 
-        for metric_name in metric_dict: 
-            metric_eval_dict[metric_name] = metric_dict[metric_name](pred_list, target_list)
-
+        metric_eval_dict = {
+            **{
+                metric_name: metric_dict[metric_name](pred_list, target_list)
+                for metric_name in metric_dict 
+            }, **{'neg_val_loss': -val_loss}
+        }
         print(metric_eval_dict)
-        return test_loss
+        return val_loss, metric_eval_dict
